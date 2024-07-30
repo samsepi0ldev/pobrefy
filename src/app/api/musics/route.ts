8e5c3859e56dbe1c0cb91728494cd6df163eee43
Elivelton Santos <samsepi0ldev@gmail.com>
2024-07-30 15:48:46 -0300

    fix: Fixed the return of song data when there is no artist

--- src/app/api/musics/route.ts
+++ src/app/api/musics/route.ts
@@ -4,26 +4,26 @@ import { NextRequest } from 'next/server'
 
 export async function GET (request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
-  const query = decodeURI(searchParams.get('search')!)
+  const query = decodeURI(searchParams.get('search')!).toLowerCase()!
 
   const yt = await Innertube.create()
 
-  const { contents } = await yt.music.search(query!, {
+  const { contents } = await yt.music.search(query, {
     type: 'song'
   }) as unknown as Root
 
 
-  const data = {
-    songs: contents[0].contents.map(({ id, artists, title, thumbnail, album, duration }) => ({
-      id,
-      title,
-      artist: artists[0].name,
-      thumbnail: thumbnail.contents[0].url,
-      album: album.name,
-      duration
-    })),
-    continuation: contents[0].continuation,
-  }
+    const data = {
+      songs: contents[0].contents.map(({ id, artists, title, thumbnail, album, duration }) => ({
+        id,
+        title,
+        artist: artists[0]?.name,
+        thumbnail: thumbnail.contents[0].url,
+        album: album.name,
+        duration
+      })),
+      continuation: contents[0].continuation,
+    }
 
   return Response.json(data)
 }
